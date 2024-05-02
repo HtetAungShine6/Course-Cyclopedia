1f2693efd653c021987be8484dd0a8dbc0268d91
“HtetAungShinehas” <htetaungshine@vasthive.fo>
2024-05-02 21:52:17 +0700

    This is the initial commit

---
+++ Course Cyclopedia/Course_CyclopediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Course_CyclopediaApp.swift
+//  Course Cyclopedia
+//
+//  Created by Akito Daiki on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Course_CyclopediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
