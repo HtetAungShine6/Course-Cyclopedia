f0f34285459bfe129b6a5dc7c6aa4938a88076ad
“HtetAungShinehas” <htetaungshine@vasthive.fo>
2024-05-15 23:42:42 +0700

    modified Authentication, checked SignIn State and App State

--- Course Cyclopedia/Configuration/AppDelegate.swift
+++ Course Cyclopedia/Configuration/AppDelegate.swift
@@ -11,16 +11,16 @@ import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     
+    //MARK: - Initializing code for Firebase (App Configuration)
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
     //MARK: - Handle the URL that application receives at the end of the authentication process
     func application(_ app: UIApplication,
                          open url: URL,
                          options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
           return GIDSignIn.sharedInstance.handle(url)
         }
-        
-    //MARK: - Initializing code for Firebase (App Configuration)
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        return true
-    }
 }
